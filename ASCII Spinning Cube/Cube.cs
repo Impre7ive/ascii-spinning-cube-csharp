a04566ed4dba46fc64c556c8dcac0b700614f5d4
Dmitry Liashenko <dmitry.liash9nko@gmail.com>
2024-02-22 15:41:11 +0200

    Add project files.

---
+++ ASCII Spinning Cube/Cube.cs
@@ -0,0 +1,30 @@
+﻿namespace ASCII_Spinning_Cube
+{
+	public class Cube
+	{
+		private int fieldWidth = 100;
+		private int fieldHeight = 26;
+		private int[] buffer;
+
+		private int sideLength;
+		private readonly int offset = 10;
+		public Cube(int sideLength)
+		{
+			this.sideLength = sideLength;
+			this.buffer = new int[fieldWidth * fieldHeight];
+		}
+
+		public void draw()
+		{
+			for (int i = 0; i < sideLength; i++)
+			{
+				for (int j = 0; j < 100; j++)
+				{
+					Console.Write('.');
+				}
+
+				Console.WriteLine();
+			}			
+		}
+	}	
+}
---
+++ ASCII Spinning Cube/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ASCII_Spinning_Cube
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var cubeSideLength = 10;
+			var cube = new Cube(cubeSideLength);
+
+			while (true)
+			{
+				cube.draw();
+				Thread.Sleep(1000/75);
+			}
+		}
+	}
+}
