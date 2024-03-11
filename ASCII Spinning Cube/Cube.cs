edef0d60b1e6a7b8828465e049a9fa82850e8c94
Dmitry Liashenko <dmitry.liash9nko@gmail.com>
2024-03-11 19:21:35 +0200

    Added the second thread for the cube render.

--- ASCII Spinning Cube/Cube.cs
+++ ASCII Spinning Cube/Cube.cs
@@ -5,13 +5,16 @@
 		private int fieldWidth = 100;
 		private int fieldHeight = 26;
 		private int[] buffer;
+		private ConsoleKey rotationDirection;
+		private ManualResetEvent stopSignal;
 
 		private int sideLength;
 		private readonly int offset = 10;
-		public Cube(int sideLength)
+		public Cube(int sideLength, ManualResetEvent stopSignal)
 		{
 			this.sideLength = sideLength;
 			this.buffer = new int[fieldWidth * fieldHeight];
+			this.stopSignal = stopSignal;
 		}
 
 		public void draw()
@@ -22,9 +25,18 @@
 				{
 					Console.Write('.');
 				}
+			}
 
-				Console.WriteLine();
-			}			
+			if (!stopSignal.WaitOne(0))
+			{
+				Thread.Sleep(1000 / 75);
+				draw();
+			}
+		}
+
+		public void changeDirection(ConsoleKey direction)
+		{
+			rotationDirection = direction;
 		}
 	}	
 }
--- ASCII Spinning Cube/Program.cs
+++ ASCII Spinning Cube/Program.cs
@@ -1,17 +1,41 @@
 ﻿namespace ASCII_Spinning_Cube
 {
-	internal class Program
+	public class Program
 	{
+		private static ManualResetEvent stopSignal = new ManualResetEvent(false);
+		private const int CubeSideLength = 10;
 		static void Main(string[] args)
 		{
-			var cubeSideLength = 10;
-			var cube = new Cube(cubeSideLength);
+			var cube = new Cube(CubeSideLength, stopSignal);
+			var cubeThread = new Thread(new ThreadStart(cube.draw));
+			cubeThread.Start();
 
-			while (true)
+			ConsoleKey key;
+
+			do
 			{
-				cube.draw();
-				Thread.Sleep(1000/75);
-			}
+				key = Console.ReadKey(true).Key;
+
+				switch (key)
+				{
+					case ConsoleKey.UpArrow:
+						cube.changeDirection(ConsoleKey.UpArrow); 
+						break;
+					case ConsoleKey.DownArrow:
+						cube.changeDirection(ConsoleKey.DownArrow);
+						break;					
+					case ConsoleKey.LeftArrow:
+						cube.changeDirection(ConsoleKey.LeftArrow);
+						break;					
+					case ConsoleKey.RightArrow:
+						cube.changeDirection(ConsoleKey.RightArrow);
+						break;
+				}
+			} 
+			while (key != ConsoleKey.Escape);
+
+			stopSignal.Set();
+			cubeThread.Join();
 		}
 	}
 }
